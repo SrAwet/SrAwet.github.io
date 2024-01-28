2561fdd41acc763150444be840ba59522ce387c7
Willian Alexander <willian.zape65@gmail.com>
2024-01-28 13:10:48 -0600

    Changes

--- funciones.js
+++ funciones.js
@@ -30,25 +30,3 @@ function easeInOutCubic(t, b, c, d) {
   t -= 2;
   return c / 2 * (t * t * t + 2) + b;
 }
-
-
-
-//Función para el form de contact
-const inputs = document.querySelectorAll(".input");
-
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
-
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-inputs.forEach((input) => {
-  input.addEventListener("focus", focusFunc);
-  input.addEventListener("blur", blurFunc);
-});
